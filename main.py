52c732743c2cfd2a0214f539ffe6db813cb5ffec
Muhammad Hasheem Shuja <muhammadhasheemshuja@gmail.com>
2026-07-18 14:17:56 +0500

    Stage 1: Route and health endpoints

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def hello():
-    return {"message":"hello, world"}
+def read_root():
+    return { "name": "Task API",
+            "version": "1.0",
+            "endpoints": ["/tasks"] 
+        }
+
+@app.get("/health")
+def health_status():
+    return {"status ":"OK"}
